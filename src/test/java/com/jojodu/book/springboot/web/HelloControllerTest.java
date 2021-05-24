c16962fe5b8abbae758a6852ceb42759c8e2971b
신승혁 <Cozak92@gmail.com>
2021-05-24 22:49:10 +0900

    2021/05/24

--- src/test/java/com/jojodu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojodu/book/springboot/web/HelloControllerTest.java
@@ -12,10 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-import static org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
+
 
 
 @ExtendWith(SpringExtension.class)
