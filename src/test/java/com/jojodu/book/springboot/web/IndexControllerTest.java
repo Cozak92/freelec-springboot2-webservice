45570dff0a3d58217f8a6ac3f7d01c2eddf06419
신승혁 <Cozak92@gmail.com>
2021-06-04 18:16:05 +0900

    2021/06/04 - 등록화면 추가

--- src/test/java/com/jojodu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojodu/book/springboot/web/IndexControllerTest.java
@@ -22,7 +22,7 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/",String.class);
 
         //then
-        assertThat(body).contains("안녕하세요. 머스테치입니다.");
+        assertThat(body).contains("안녕하세요. 머스테치입니다");
     }
 
 }
